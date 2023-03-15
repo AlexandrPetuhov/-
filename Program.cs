e9c4ffe043797994fa725663d2b93cadcae1992e
Alexandr <Nefilim228@gmail.com>
2023-03-15 20:10:10 +0500

    Написание кода по условиям задачи

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] array = { "Hello", "Alexandr", "Perm", "GB", "27", "Text", "!", ")" };
+string arr = string.Join(", ", array);
+Console.Write($"[{arr}] -> ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        if (i != array.Length - 1)
+        {
+            Console.Write($"{array[i]}, " );           
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+}
